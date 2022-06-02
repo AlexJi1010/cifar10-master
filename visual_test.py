10d6e21bca1f49750a62339a56defa3fa907bc06
AlexJi1010 <Alexji3066@gmail.com>
2022-06-02 13:56:09 -0700

    WIP

--- visual_test.py
+++ visual_test.py
@@ -9,7 +9,8 @@ from vgg16modified import Network as MVGG16Net
 from utils import device, get_all_preds, get_num_correct
 import matplotlib.pyplot as plt
 import numpy as np
-
+from matplotlib.pyplot import figure
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 def run():
     torch.multiprocessing.freeze_support()
     transform = transforms.Compose([
@@ -24,18 +25,22 @@ def run():
         download=False,
         transform=transform
     )
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=100, num_workers=1)
-
+    print('loading test data')
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=400, num_workers=1)
+    print('test data loaded')
     # load the model with least validation loss
+    print('loading model')
     model = MVGG16Net(torchvision.models.vgg16(pretrained=True)).to(device)
     model.load_state_dict(
         torch.load('models/model-transferlr_vgg16.pth',
                    map_location=device)
     )
     model
+    print('evaluating model')
     model.eval()
     # obtain one batch of test images
     images, labels = next(iter(test_loader))
+
     images = images.numpy()
     test_preds = get_all_preds(model, test_loader)
     test_stacked = torch.stack(
@@ -44,9 +49,9 @@ def run():
     )
     # plot the images in the batch, along with predicted and true labels
     # plot format: "predicted-label (true-label)"
-    fig = plt.figure(figsize=(25, 20))
-    for i in np.arange(100):
-        ax = fig.add_subplot(10, 10, i + 1, xticks=[], yticks=[])
+    fig = plt.figure(figsize=(30, 30))
+    for i in np.arange(400):
+        ax = fig.add_subplot(20, 20, i + 1, xticks=[], yticks=[])
         image = np.squeeze(np.transpose(images[i], (1, 2, 0)))  # B*C*H*W --> H*W*C
         image = image * np.array((0.229, 0.224, 0.225)) + \
                 np.array((0.485, 0.456, 0.406))  # un-normalize the image
@@ -54,9 +59,11 @@ def run():
         ax.imshow(image)
         ax.set_title(f'{test_set.classes[test_stacked[i, 1].item()]} ({test_set.classes[labels[i].item()]})',
                      color=('green' if test_stacked[i, 1] == labels[i] else 'red'))
-
+    plt.tight_layout()
     plt.show()
-    fig.savefig('visualizations/testest.png', bbox_inches='tight')
+    test_correct = get_num_correct(test_preds, torch.as_tensor(test_set.targets))
+    print(f'Test Correct: {test_correct:6}\tTest Accuracy: {(100 * test_correct / len(test_set)):6.2f}%')
+    fig.savefig('visualizations/testest2.png', bbox_inches='tight')
     plt.close()
     print('done')
 
